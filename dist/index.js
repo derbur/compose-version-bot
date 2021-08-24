b6788853eb69f77a515d47a415d08c25fa0feb3b
derbur <dburns921@gmail.com>
2021-08-23 23:23:17 -0400

    cleanup

--- dist/index.js
+++ dist/index.js
@@ -6317,29 +6317,27 @@ const core = __nccwpck_require__(507);
 const github = __nccwpck_require__(322);
 const fs = __nccwpck_require__(747);
 
-try {
-  // who-to-greet defined in input metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`hello ${nameToGreet}`);
-  const time = (new Date()).toTimeString();
-  core.setOutput('time', time);
-  // get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`the event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+// try {
+//   // who-to-greet defined in input metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`hello ${nameToGreet}`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput('time', time);
+//   // get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2);
+//   console.log(`the event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
 
 try {
-  const files = fs.readdirSync('.');
-  console.log(`current directory: ${files}`);
   console.log('updating image...');
   const imageConfig = JSON.parse(fs.readFileSync('./test/data/image-config.json', { encoding: 'utf-8' }));
   const imageRegex = /test_registry\/image_name:(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/g;
   const imageString = `${imageConfig.registry}/${imageConfig.image}:${imageConfig.tag}`;
   const compose = fs.readFileSync('./test/data/docker-compose.yml', { encoding: 'utf-8' });
   const updatedCompose = compose.replace(imageRegex, imageString);
-  console.log(updatedCompose);
+  console.log('updated compose\n', updatedCompose);
 } catch (error) {
   core.setFailed(error.message);
 }
