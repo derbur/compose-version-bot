960d0b86cb483d95ac0ed39c05be3a8843f5ce68
derbur <dburns921@gmail.com>
2021-08-23 22:38:19 -0400

    more

--- dist/index.js
+++ dist/index.js
@@ -6336,10 +6336,10 @@ try {
   const upLevel = fs.readdirSync('../');
   console.log(`level up directory: ${upLevel}`);
   console.log('updating image...');
-  const imageConfig = JSON.parse(fs.readFileSync('../test/data/image-config.json', { encoding: 'utf-8' }));
+  const imageConfig = JSON.parse(fs.readFileSync('./data/image-config.json', { encoding: 'utf-8' }));
   const imageRegex = new RegExp(`^${imageConfig.registry}\/${imageConfig.image}:(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`, 'g');
   const imageString = `${imageConfig.registry}/${imageConfig.image}:${imageConfig.tag}`;
-  const compose = fs.readFileSync('../test/data/docker-compose.yml', { encoding: 'utf-8' });
+  const compose = fs.readFileSync('./data/docker-compose.yml', { encoding: 'utf-8' });
   const updatedCompose = compose.replace(imageRegex, imageString);
   console.log(updatedCompose);
 } catch (error) {
